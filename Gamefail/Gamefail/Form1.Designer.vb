9cf153829c4aa5dc9d3671e61871da9bce29fc5e
ordelore <ordelore@gmail.com>
2014-09-15 16:19:53 -0500

    First commit for Visual Bird
    The bird only flaps up and down. Mostly up.

---
+++ Gamefail/Gamefail/Form1.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.picBird = New System.Windows.Forms.PictureBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.picBird, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'picBird
+        '
+        Me.picBird.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.picBird.Image = Global.Gamefail.My.Resources.Resources.flapbird1
+        Me.picBird.Location = New System.Drawing.Point(190, 210)
+        Me.picBird.Name = "picBird"
+        Me.picBird.Size = New System.Drawing.Size(66, 53)
+        Me.picBird.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picBird.TabIndex = 0
+        Me.picBird.TabStop = False
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.Gamefail.My.Resources.Resources.jprgqobw
+        Me.ClientSize = New System.Drawing.Size(464, 480)
+        Me.Controls.Add(Me.picBird)
+        Me.Name = "frmMain"
+        Me.Text = "Flappy Bird"
+        CType(Me.picBird, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents picBird As System.Windows.Forms.PictureBox
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+
+End Class
---
+++ Gamefail/Gamefail/Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class frmMain
+
+
+    Private Sub Form1_MouseClick(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Me.MouseClick
+        'Make bird go up
+        For Z = 1 To 25 Step 1
+            picBird.Location = New Point(picBird.Location.X, picBird.Location.Y - 1)
+        Next Z
+    End Sub
+
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        While Y > 465
+
+        End While
+    End Sub
+End Class
